27d72f4a426d5d7796f7547cf4d5a55b85683ef7
Juan Barriteau <juan@barriteau.net>
2018-11-27 06:44:53 -0400

    City Bikes in Miami. Number of bikes per station. Updates every second. Markers added. Popup shows number of free bikes.

--- citybike-client/src/App.js
+++ citybike-client/src/App.js
@@ -1,40 +1,67 @@
-import React, { Component } from "react";
-import socketIOClient from "socket.io-client";
-import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import React, { Component } from 'react';
+import socketIOClient from 'socket.io-client';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+const moment = require('moment');
 
 class App extends Component {
-  constructor() {
-    super();
 
-    this.state = {
-      response: false,
-      endpoint: "http://127.0.0.1:4001",
-      lat: 51.505,
-      lng: -0.09,
-      zoom: 13
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            response: [],
+            endpoint: 'http://127.0.0.1:4001',
+            lat: 25.82,
+            lng: -80.19,
+            zoom: 12
+        };
+    }
 
-  }
-  componentDidMount() {
-    const { endpoint } = this.state;
-    const socket = socketIOClient(endpoint);
-   
-  }
-  render() {
-    const { response } = this.state;
-    const position = [this.state.lat, this.state.lng]
-    return (
+    componentDidMount() {
+        const { endpoint } = this.state;
+        const socket = socketIOClient(endpoint);
+        socket.on('connect', function() {
+            console.log(`Connected to server`);
+        });
+        socket.on('MiamiStations', data =>
+            this.setState({ response: data })
+        );
+    }
 
-      <div className="map">
-        <h1> City Bikes in Miami </h1>
-        <Map center={position} zoom={this.state.zoom}>
-          <TileLayer
-            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-        </Map>
-      </div>
-    );
-  }
+    render() {
+        const response = this.state.response;
+        const position = [this.state.lat, this.state.lng];
+        return (
+            <div className="map">
+                <h1>City Bikes in Miami</h1>
+                <Map center={position} zoom={this.state.zoom}>
+                    <TileLayer
+                        attribution='&amp;copy <a href="//osm.org/copyright">OpenStreetMap</a> contributors'
+                        url="//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    { response.length === 0 ? <span id="loading">Loading...</span> : <Markers markers={response} /> }
+                </Map>
+            </div>
+        );
+    }
 }
+
+const Markers = ({ map, markers }) => {
+    const items = markers.map(({ id, ...props }) => (
+        <Popups map={map} key={id} {...props} />
+    ));
+    return <div style={{display: 'none'}}>{items}</div>;
+};
+
+const Popups = ({ map, free_bikes, id, latitude, longitude, name, timestamp }) => (
+    <Marker map={map} position={[latitude, longitude]}>
+        <Popup>
+            <span>
+                <b>{free_bikes} free bikes</b> <code>{moment(timestamp).format('HH:mm:ss[ hrs]')}</code>
+                <br />
+                {name} Station
+            </span>
+        </Popup>
+    </Marker>
+);
+
 export default App;
--- citybike-server/server.js
+++ citybike-server/server.js
@@ -1,14 +1,10 @@
-const express = require("express");
-const http = require("http");
-const socketIo = require("socket.io");
-const axios = require("axios");
-
-
-
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const axios = require('axios');
 const port = process.env.PORT || 4001;
-const index = require("./routes/index");
+const index = require('./routes/index');
 const app = express();
-
 
 app.use(index);
 
@@ -16,31 +12,38 @@ const server = http.createServer(app);
 const io = socketIo(server); // < Interesting!
 let interval;
 
-io.on("connection", socket => {
-  var socketId = socket.id;
-  var clientIp = socket.request.connection.remoteAddress;
-  console.log('New connection ' + socketId + ' from ' + clientIp);
-  if (interval) {
-    clearInterval(interval);
-  }
-  interval = setInterval(() => getApiAndEmit(socket), 1000);
-  socket.on("disconnect", () => {
-    console.log("Client disconnected");
-  });
-});
-
-const getApiAndEmit = async socket => {
-  try {
-    const res = await axios.get(
-      "http://api.citybik.es/v2/networks/decobike-miami-beach"
-    );
-    //socket.emit("FromAPI", res.data.currently.temperature);
-  } catch (error) {
-    console.error(`Error: ${error.code}`);
-  }
+const fetchMiamiStations = async socket => {
+    try {
+        const res = await axios.get(
+            'http://api.citybik.es/v2/networks/decobike-miami-beach'
+        );
+        socket.emit('MiamiStations', res.data.network.stations);
+    } catch(error) {
+        console.error(`Error: ${error.code}`);
+    }
 };
 
-server.listen(port, () => console.log(`Listening on port ${port}`));
+io.on('connection', socket => {
+    let socketId = socket.id;
+    let clientIp = socket.request.connection.remoteAddress;
+    console.log(`New connection ${socketId} from ${clientIp}`);
 
+    if (interval) {
+        clearInterval(interval);
+    }
+    interval = setInterval(
+        () => fetchMiamiStations(socket),
+        1000
+    );
+    socket.on(
+        'disconnect',
+        () => {
+            console.log(`Client disconnected`);
+        }
+    );
+});
 
-
+server.listen(
+    port,
+    () => console.log(`Listening on port ${port}`)
+);
