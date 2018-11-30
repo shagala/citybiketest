a4089ba15ff8523b90adb6b9a45c61949782efcf
Alexander Castellanos <xan217@hotmail.com>
2018-11-30 16:48:38 -0500

    CityBike development Test by Renne Castellanos
    index.html: 
     - Updated stylesheet dependency to use the lastest markers.
    index.css: 
     - Added a class for the developer info
    server.js: 
     - Added listener for the request from the client
     - Added controls for the data
     - Edited request for the eternal server to clean the data before save it and pass it to the user
    App.js:
     - Added data structures for the easy handle of the markers, based in the Leaflet Documentation
     - Edited marker structure to adapt it to the test request
     - Edited default state of the class to add the markers field
     - Added the request to the local server
     - Added handleResponse to structure the data for the markers
     - Added component to handle the markers view
    
    Considerations:
     - Most of the process to solve this test was learning about the library, I've never used before.
     - The documentation is very long and specific and it allows to learn the basics about the technology in very short time.
     - Very interesting way to develop a view and serve data to the client, very happy to learn about an unknown technology until I talk with your company.
     - Very interested in keep learning about the technology, the opportunities for this way of development are too many.
     - Hard to master ALL abut the library, it have too many ways to do the same thing but the documentation is a great support tool.
     - Thanks for the opportunity in any case.

--- citybike-server/server.js
+++ citybike-server/server.js
@@ -1,45 +1,77 @@
+/**
+ * server.js
+ * version: 1.0 by Renne Castellanos
+ * 
+ * Last version of functional server file to consult to the web API
+ *    for the bikes registered in the provider server, and retrieve
+ *    the specific data of interest to the client to reduce the 
+ *    communication and loading time.
+ */
+
+// Defautl constants
+const axios = require("axios");
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
-const axios = require("axios");
-
-
 
 const port = process.env.PORT || 4001;
 const index = require("./routes/index");
 const app = express();
 
-
 app.use(index);
 
+// Server creation proccess
 const server = http.createServer(app);
-const io = socketIo(server); // < Interesting!
+const io = socketIo(server);
+
+// Consulting time local variable definition
 let interval;
 
+// Listener of the connection
 io.on("connection", socket => {
+
+  // Local variables
   var socketId = socket.id;
   var clientIp = socket.request.connection.remoteAddress;
+
+  // Control log
   console.log('New connection ' + socketId + ' from ' + clientIp);
+
+  // Interval verification
   if (interval) {
     clearInterval(interval);
   }
   interval = setInterval(() => getApiAndEmit(socket), 1000);
+
+  // Listener of diconnection
   socket.on("disconnect", () => {
     console.log("Client disconnected");
   });
+
+  // Listener of the request for the bikes consult
+  socket.on("FromAPI", (data) => { socket.broadcast.emit('FromAPI', data) });
 });
 
+// Default provided asyncronus function with a modification to set the data
+//    at the moment it is retrieved.
 const getApiAndEmit = async socket => {
   try {
+
+    // Request to the provider
     const res = await axios.get(
       "http://api.citybik.es/v2/networks/decobike-miami-beach"
     );
-    //socket.emit("FromAPI", res.data.currently.temperature);
+
+    // Definition of the data to be retrieved to reduce the load in the client
+    data = res.data.network.stations;
   } catch (error) {
+
+    // Log the error
     console.error(`Error: ${error.code}`);
   }
 };
 
+// Server listener
 server.listen(port, () => console.log(`Listening on port ${port}`));
 
 
