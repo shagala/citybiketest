d622f1ff808b1827b395576c4acb1bd9d14e53a7
Andres Buelvas <afroelectrico@gmail.com>
2019-03-29 11:46:52 -0400

    PR test completed Andres

--- citybike-client/src/App.js
+++ citybike-client/src/App.js
@@ -1,7 +1,9 @@
+// Dependencias
 import React, { Component } from "react";
 import socketIOClient from "socket.io-client";
-import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import { Map, TileLayer, Marker, Popup, CircleMarker, Circle } from "react-leaflet";
 
+// Aplicacion
 class App extends Component {
   constructor() {
     super();
@@ -9,25 +11,61 @@ class App extends Component {
     this.state = {
       response: false,
       endpoint: "http://127.0.0.1:4001",
-      lat: 51.505,
-      lng: -0.09,
-      zoom: 13
+      lat: 25.790654,
+      lng: -80.1300455,
+      zoom: 12.5,
+      estaciones: null
     };
-
   }
+
   componentDidMount() {
     const { endpoint } = this.state;
-    const socket = socketIOClient(endpoint);
-   
+    try {
+      const socket = socketIOClient(endpoint); // establece conexion con el servidor
+      socket.on('mensaje', (data) => {
+        this.setState({
+          estaciones : data, // el evento 'mensaje' es capturado al conectarse
+        })
+      });
+    }
+    catch (error) {
+      console.error(`Errorrrrrrrr client: ${error.code}`);
+    }
   }
+
+  // Funcion para renderizar los datos de disponibilidad
+  obtenerMarkers = () => {
+    var stations_bikes = this.state.estaciones.network.stations.map( //mapeamos el div para cada estacion
+      (station) =>{
+        return (
+          <div className="map2">
+            <Circle center={[station.latitude, station.longitude]} color="blue" radius={3}/>
+            <CircleMarker center={[station.latitude, station.longitude]} color="orange" radius={8}>
+            </CircleMarker>   
+            <Marker key={station.id} position={[station.latitude, station.longitude]}>
+              <Popup>
+                <b> Nombre: </b> {station.name ? station.name : ''} <br/>
+                <b> Direccion:</b> {station.extra.address ? station.extra.address : ''} <br/>
+                <b> Puestos vacios: </b>{station.empty_slots ? station.empty_slots : ''} <br/>
+                <b> Bicicletas libres: </b>{station.free_bikes ? station.free_bikes : ''} <br/>
+              </Popup>
+            </Marker>
+          </div>
+        );
+      });
+    return stations_bikes;
+  };
+
   render() {
     const { response } = this.state;
-    const position = [this.state.lat, this.state.lng]
+    const position = [this.state.lat, this.state.lng];
+
     return (
 
       <div className="map">
         <h1> City Bikes in Miami </h1>
         <Map center={position} zoom={this.state.zoom}>
+          {this.state.estaciones ? this.obtenerMarkers() : null}
           <TileLayer
             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
--- citybike-server/server.js
+++ citybike-server/server.js
@@ -1,33 +1,47 @@
+// Dependencias
 const express = require("express");
+const axios = require("axios")
 const http = require("http");
 const socketIo = require("socket.io");
+
+// api
 const citybikeurl = "http://api.citybik.es/v2/networks/decobike-miami-beach"
 
+const port = process.env.PORT || 4001; //puerto a utilizar
+const index = require("./routes/index"); //renderizar
+const app = express(); // crea una apliacion express
 
+app.use(index); // usar esta ruta
 
-const port = process.env.PORT || 4001;
-const index = require("./routes/index");
-const app = express();
-
-
-app.use(index);
-
-const server = http.createServer(app);
-const io = socketIo(server); // < Interesting!
+// Creacion de servidor y socket
+const server = http.createServer(app); // instancia del http.server
+const io = socketIo(server); //  socketio del server
 let interval;
 
+// Establecemos conexion del socket
 io.on("connection", socket => {
-  var socketId = socket.id;
-  var clientIp = socket.request.connection.remoteAddress;
-  console.log('New connection ' + socketId + ' from ' + clientIp);
-  socket.on("disconnect", () => {
-    console.log("Client disconnected");
-  });
+	var socketId = socket.id;
+	var clientIp = socket.request.connection.remoteAddress;
+
+	console.log('New connection ' + socketId + ' from ' + clientIp); // conexion satisfactoria
+
+	interval = setInterval( () => obtener_api_enviar(socket), 3000); // ejecutar la funcion cada 3000 milisegundos
+
+	socket.on("disconnect", () => {
+		console.log("Client disconnected");
+	});
 });
 
-
+// usando async/await
+const obtener_api_enviar = async socket => {
+	try {
+		const res = await axios.get(citybikeurl); // api request (modo asincronico/espera)
+		socket.emit('mensaje', res.data) // enviar a cliente
+		//console.log(res.data.network.stations);
+	} 
+	catch (error) {
+		console.error(`Errorrrrrrrr server: ${error.code}`);
+	}
+};
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
-
-
-
