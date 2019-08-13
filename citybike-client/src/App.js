f14a327ad0ef52169ed504ee5c0e45ec8c9d719c
Guillermo Betancourt <guillermojbetancourt@gmail.com>
2019-08-13 12:14:52 -0400

    Server and client ready

--- citybike-client/src/App.js
+++ citybike-client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import socketIOClient from "socket.io-client";
 import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import './App.css'
 
 class App extends Component {
   constructor() {
@@ -9,29 +10,55 @@ class App extends Component {
     this.state = {
       response: false,
       endpoint: "http://127.0.0.1:4001",
-      lat: 51.505,
-      lng: -0.09,
-      zoom: 13
+      lat: 25.790654,
+      lng: -80.1300455,
+      zoom: 13,
+      stations: []
     };
-
   }
+
   componentDidMount() {
     const { endpoint } = this.state;
     const socket = socketIOClient(endpoint);
-   
+    
+    socket.on('location', (location) => {
+      this.setState({
+        lat: location.latitude,
+        lng: location.longitude
+      });
+    });
+    
+    socket.on('stations', (stations) => {
+      this.setState({
+        stations: stations
+      });
+    });
   }
-  render() {
-    const { response } = this.state;
-    const position = [this.state.lat, this.state.lng]
-    return (
 
+  renderMarkers() {
+    return this.state.stations.map((station) => 
+      <Marker key={station.id} position={[station.latitude, station.longitude]}>
+        <Popup>
+          <div> Address: { station.extra.address } </div>
+          <div> Bikes availables: { station.free_bikes }</div>
+          <div> Empty slots: { station.empty_slots }</div>
+        </Popup>
+      </Marker>
+    );
+  }
+
+  render() {
+    const { lat, lng, zoom } = this.state;
+
+    return (
       <div className="map">
-        <h1> City Bikes in Miami </h1>
-        <Map center={position} zoom={this.state.zoom}>
+        <h1 className='title'> City Bikes in Miami </h1>
+        <Map center={[lat, lng]} zoom={zoom}>
           <TileLayer
             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
+          { this.renderMarkers() }
         </Map>
       </div>
     );
--- citybike-server/server.js
+++ citybike-server/server.js
@@ -3,31 +3,48 @@ const http = require("http");
 const socketIo = require("socket.io");
 const citybikeurl = "http://api.citybik.es/v2/networks/decobike-miami-beach"
 
-
-
 const port = process.env.PORT || 4001;
 const index = require("./routes/index");
 const app = express();
-
 
 app.use(index);
 
 const server = http.createServer(app);
 const io = socketIo(server); // < Interesting!
-let interval;
+let interval = 5000;
 
 io.on("connection", socket => {
   var socketId = socket.id;
   var clientIp = socket.request.connection.remoteAddress;
   console.log('New connection ' + socketId + ' from ' + clientIp);
+  
+  setInterval(() => {
+    http.get(citybikeurl, (response) => {
+      var data = Buffer.alloc(0);
+
+      response.on('data', (dataChunk) => {
+        data = Buffer.concat([data, dataChunk]);
+      })
+
+      response.on('end', () => {
+        data = JSON.parse(data.toString());
+        console.log(`---\nCityBike response status code: ${response.statusCode}`)
+        console.log(`Location: ${data.network.location.city}`);
+        console.log(`Number of stations: ${data.network.stations.length}`);
+
+        io.emit('location', data.network.location);
+        io.emit('stations', data.network.stations);
+      });
+
+    }).on('error', (error) => {
+      console.log(error);
+    }).end();
+  }, interval);
+
   socket.on("disconnect", () => {
     console.log("Client disconnected");
   });
 });
 
 
-
 server.listen(port, () => console.log(`Listening on port ${port}`));
-
-
-
