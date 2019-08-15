2f103fd87eaedfbf07f90e55b6bd2de51bc36007
ritces <ritces_parra@hotmail.com>
2019-08-14 22:23:34 -0400

    listo leer api

--- citybike-server/server.js
+++ citybike-server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
 const citybikeurl = "http://api.citybik.es/v2/networks/decobike-miami-beach"
+const axios = require("axios");
 
 
 
@@ -20,10 +21,30 @@ io.on("connection", socket => {
   var socketId = socket.id;
   var clientIp = socket.request.connection.remoteAddress;
   console.log('New connection ' + socketId + ' from ' + clientIp);
+
+  if (interval) {
+    clearInterval(interval);
+  }
+  // Se establece el intervalo  en 6000 ms 
+  interval = setInterval(() => obtenerApi(socket), 6000);
   socket.on("disconnect", () => {
     console.log("Client disconnected");
   });
 });
+
+// Se obtiene la informacion de la Api
+const obtenerApi = async socket => {
+
+  try {
+    const mensaje  = await axios.get(citybikeurl);  
+    socket.emit("cityBici", mensaje.data)  
+    console.log(mensaje.data.network.stations)
+
+  } catch (error) {
+
+    console.error(`Errorr : ${error.code}`);
+  }
+};
 
 
 
