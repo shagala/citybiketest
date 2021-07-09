f85c0b1119b11c9849a095873263f8bc3071cfcc
Ivan D. Sanchez <cosmosoft.soluciones@gmail.com>
2021-07-08 23:49:09 -0500

    App complete

--- citybike-server/routes/index.js
+++ citybike-server/routes/index.js
@@ -1,6 +1,14 @@
+const { response } = require("express");
 const express = require("express");
 const router = express.Router();
+const fetch = require('node-fetch');
+const citybikeurl = "http://api.citybik.es/v2/networks/decobike-miami-beach"
+
 router.get("/", (req, res) => {
-  res.send({ response: "I am alive" }).status(200);
+   
+  fetch(citybikeurl)
+  .then(res => res.json())
+  .then(json => res.send({response: json, message: "Request Ok!!!"}).status(200));
 });
+
 module.exports = router;
