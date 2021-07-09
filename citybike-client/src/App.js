0fd3ebe6f313207113f4316d063d1532f512f72e
Ivan D. Sanchez <cosmosoft.soluciones@gmail.com>
2021-07-08 23:36:21 -0500

    app finished

--- citybike-client/src/App.js
+++ citybike-client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import socketIOClient from "socket.io-client";
 import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import { imageOverlay } from "leaflet";
 
 class App extends Component {
   constructor() {
@@ -8,30 +9,53 @@ class App extends Component {
 
     this.state = {
       response: false,
-      endpoint: "http://127.0.0.1:4001",
-      lat: 51.505,
-      lng: -0.09,
-      zoom: 13
+      endpoint: "http://127.0.0.1:4001/",
+      lat: 25.797651558015108,
+      lng: -80.16283456162486,
+      zoom: 13,
+      stations:[]
     };
 
   }
+  
   componentDidMount() {
+    const self= this
     const { endpoint } = this.state;
     const socket = socketIOClient(endpoint);
+    socket.on("citybike", function(data){
+      const result = JSON.parse(data)
+      self.setState({stations: result.network.stations})
+    })
+
    
   }
   render() {
-    const { response } = this.state;
+    const { stations } = this.state;
     const position = [this.state.lat, this.state.lng]
     return (
 
       <div className="map">
         <h1> City Bikes in Miami </h1>
+          {console.log(stations)}
         <Map center={position} zoom={this.state.zoom}>
           <TileLayer
             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
+          {
+            stations.length > 0 && stations.map((station)=>(
+              <Marker key={station.id} position={[station.latitude, station.longitude]}>
+                {console.log(station)}
+                <Popup>
+                  <h2>{station.name}</h2>
+                  <h3>Empty slots: {station.empty_slots}</h3>
+                </Popup>
+
+              </Marker>
+            ))
+          }
+          
+          
         </Map>
       </div>
     );
